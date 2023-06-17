946609d176db73a731cd37609b34d461c6b2aa07
Davidrami12 <davidrami2000@gmail.com>
2023-06-17 16:04:52 +0200

    Initialize swiper

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -16,3 +16,24 @@ const handleClick = () => {
 mobileMenuBurger.addEventListener("click", handleClick);
 
 // SWIPER
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
